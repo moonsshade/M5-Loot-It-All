b50e7b326a446d528a2887d91ac47c6796ad328e
VeeTheWitchy <125075349+moonsshade@users.noreply.github.com>
2025-12-12 13:32:31 -0800

    addthrusteraudio

--- lessons/item.gd
+++ lessons/item.gd
@@ -1,8 +1,21 @@
 extends Area2D
+@onready var sfx_player: AudioStreamPlayer = _find_sfx_player()
 
+func _find_sfx_player() -> AudioStreamPlayer:
+	for child in get_children():
+		if child is AudioStreamPlayer:
+			return child
+	return null
 
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	get_node("CollisionShape2D").set_deferred("disabled", true)
+	visible = false
+	
+	if sfx_player:
+		sfx_player.play()
+		await sfx_player.finished
+	
+	queue_free()
+		
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
-	
-func _on_area_entered(area_that_entered: Area2D) -> void:
-	queue_free()
--- lessons/ship.gd
+++ lessons/ship.gd
@@ -1,18 +1,22 @@
 extends Area2D
 
-var health:=10
-var max_speed := 1500.0
-var velocity := Vector2(0, 0)
-var steering_factor := 6
+
 var gem_count := 0
+var max_speed := 1200.0
+var velocity := Vector2(0, 0)
+var steering_factor := 3.0
+var health := 10
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	set_health(health)
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
-	var viewport_size := get_viewport_rect().size
-	position.x = wrapf(position.x, 0, viewport_size.x)
-	position.y = wrapf(position.y, 0, viewport_size.y)
+	
+
 
 	if direction.length() > 1.0:
 		direction = direction.normalized()
@@ -22,23 +26,25 @@ func _process(delta: float) -> void:
 	velocity += steering * steering_factor * delta
 	position += velocity * delta
 	
+	var viewport_size := get_viewport_rect().size
+	position.x = wrapf(position.x, 0,viewport_size.x)
+	position.y = wrapf(position.y, 0, viewport_size.y)
+
 	if velocity.length() > 0.0:
+		#rotation = velocity.angle()
 		get_node("Sprite2D").rotation = velocity.angle()
-		
-func set_health(new_health: int) -> void:
+
+func set_health(new_health: int) ->void:
 	health = new_health
-	get_node("UI/HealthBar").value = health 
-	
-func _ready() -> void:
-	area_entered.connect(_on_area_entered)
-	set_health(50)
-	
+	get_node("UI/HealthBar").value = health
+
 func _on_area_entered(area_that_entered: Area2D) -> void:
 	if area_that_entered.is_in_group("gem"):
-		set_gem_count(gem_count + 1)
-	elif area_that_entered.is_in_group("healing_item"):
+		set_gem_count(gem_count +1)
+	elif area_that_entered.is_in_group("healing_item"): 
 		set_health(health + 10)
- 
+	
 func set_gem_count(new_gem_count: int) -> void:
 	gem_count = new_gem_count
 	get_node("UI/GemCount").text = "x" + str(gem_count)
+	
